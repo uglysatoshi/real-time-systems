a67cf1af45cd6074309df6ca1b77890cca6499ee
Satoshi <118019529+uglysatoshi@users.noreply.github.com>
2025-03-09 16:30:37 +0300

    Create result-functions.sh

---
+++ lab2/result-functions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ $# -ne 3 ]; then
+  echo "Использование: $0 <число1> <число2> <число3>"
+  exit 1
+fi
+average() {
+  local sum=$(( $1 + $2 + $3 ))
+  echo "Среднее значение: $(echo "$sum / 3" | bc -l)"
+}
+average $1 $2 $3 > result_functions.txt
