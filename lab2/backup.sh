9a49a8e4912a360fc548a332e94b17493694abd8
Satoshi <118019529+uglysatoshi@users.noreply.github.com>
2025-03-26 18:31:11 +0300

    Update backup.sh

--- lab2/backup.sh
+++ lab2/backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-backup_dir="/home/valery"
+backup_dir="/home/lab2/results"
 mkdir -p $backup_dir
 date_str=$(date +"%Y%m%d_%H%M%S")
 tar -czf "$backup_dir/backup_$date_str.tar.gz" /etc/network
