bc6b074a0348e2d3d2ba4b1941e86fa0f800d819
Satoshi <118019529+uglysatoshi@users.noreply.github.com>
2025-03-26 18:31:54 +0300

    Update result-loops.sh

--- lab2/result-loops.sh
+++ lab2/result-loops.sh
@@ -8,4 +8,4 @@ sum=0
 for ((i=1; i<=N; i++)); do
   sum=$((sum + i))
 done
-echo "Сумма чисел от 1 до $N: $sum" > result_loops.txt
+echo "Сумма чисел от 1 до $N: $sum" > /home/lab2/results/result_loops.txt
