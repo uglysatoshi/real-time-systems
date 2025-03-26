4da72cea887b62d82ea2f994a616c2b0d8b38d4e
Satoshi <118019529+uglysatoshi@users.noreply.github.com>
2025-03-26 18:55:52 +0300

    Update result-functions.sh

--- lab2/result-functions.sh
+++ lab2/result-functions.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+
 if [ $# -ne 3 ]; then
   echo "Использование: $0 <число1> <число2> <число3>"
   exit 1
 fi
+
 average() {
   local sum=$(( $1 + $2 + $3 ))
-  echo "Среднее значение: $(echo "$sum / 3" | bc -l)"
+  local avg=$(echo "scale=2; $sum / 3" | bc -l)
+  echo "Среднее значение: $avg"
 }
-average $1 $2 $3 > /home/lab2/results/result_functions.txt
+
+average "$1" "$2" "$3" > /home/lab2/results/result_functions.txt
